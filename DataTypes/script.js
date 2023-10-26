d25f9b75f2e9d063cc5071ba337c194776e7377d
Harpreet <harpreetsingh25102001@gmail.com>
2023-10-25 17:22:13 -0700

    Data types

---
+++ DataTypes/script.js
@@ -0,0 +1,17 @@
+// There are two types of data types in Javascript.
+
+// 1.Primitive
+// 2.Non-Primitive (Reference)
+
+// Primitive
+// 1) String
+// 2) Number
+// 3) BigInt
+// 4) Symbol
+// 5) Undefined
+// 6) String
+// 7) Null
+
+// Non-Primitive (Reference)
+// 1)Array
+// 2)Object
