621fdfb3568c79c5080b63217e5193854e705b0a
Harpreet <harpreetsingh25102001@gmail.com>
2023-10-26 16:25:10 -0700

    string and string methods

---
+++ String/script.js
@@ -0,0 +1,26 @@
+// A JavaScript string is zero or more characters written inside quotes.
+
+let text = "John Doe";
+
+// The length property returns the length of a string
+
+let length = text.length;
+
+// There are many methods we can use with strings like down below
+
+text.slice();
+text.substring();
+text.substr();
+text.replace();
+text.replaceAll();
+text.toUpperCase();
+text.toLowerCase();
+text.concat();
+text.trim();
+text.trimStart();
+text.trimEnd();
+text.padStart();
+text.padEnd();
+text.charAt();
+text.charCodeAt();
+text.split();
