88d6ea3c4adad0a37d105c429700a495ceb490fc
Harpreet <harpreetsingh25102001@gmail.com>
2023-10-29 20:14:03 -0700

    logical operator description

--- LogicalOperators/script.js
+++ LogicalOperators/script.js
@@ -1,5 +1,5 @@
 // There are three types of logical operators.
 
-// 1) &&
-// 2) ||
-// 3) !
+// 1) && - Both expressions have to be true
+// 2) || - One expression has to be true
+// 3) !  - Not operator
