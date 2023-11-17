4f439561ee6efecc65b3cc5a2a5502596037b187
Harpreet <harpreetsingh25102001@gmail.com>
2023-11-16 21:48:04 -0800

    Loops with arrays

--- 3-Master Objects/5-Destructuring Object/index.js
+++ 3-Master Objects/5-Destructuring Object/index.js
@@ -13,3 +13,7 @@ const { brand, model } = vehicleOne;
 // If we want to change the names of variables we can do it like this.
 
 const { brand: brandName, model: modelName } = vehicleOne;
+
+// We can also make default variable with this. If we dont have location in this Object it will automatically set to WA.
+
+const { brands: brandNames, models: modelNames, locations = "WA" } = vehicleOne;
---
+++ 4-Master Loops/7-Loops With Arrays/index.js
@@ -0,0 +1,13 @@
+// This will be done only by for loop because that is what we use most of the time.
+
+var arr = [1, 2, 3, 4, 5, 6];
+
+for (let i = 0; i <= arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// First i will be 0
+// It will check the condition if 0 < arr.length which is 6
+// It will console.log the index of array which is i that is 0 right now
+// Then it will increase the i by one
+// The whole process repeats until the condition is not correct
