c5a0725d3a188e795fddd322341b356c1706055b
Harpreet <harpreetsingh25102001@gmail.com>
2024-01-04 17:30:30 -0800

    mouse events

---
+++ 7-Master DOM/15-Mouse Events/index.js
@@ -0,0 +1,14 @@
+// There are many types of mouse events in Javascript.
+
+// 1.click         => when we click on something
+// 2.dblclick      => when we double click on something
+// 3.mouseup       => when we release the key
+// 4.mousedown     => when key presses down
+// 5.mouseenter    => when we hover over some element
+// 6.mouseover     => when we hover over some element (for ancestors too)
+// 7.mouseleave    => when we leave the element
+// 8.mouseout      => when we leave the element (for ancestors too)
+// 9.wheel         => when we use the wheel of mouse
+// 10.scroll       => when we scroll (not a mouse event)
+// 11.touch        => when we touch (not a mouse event , only works on mobile)
+// 12.drag         => when we drag an element (fist make the element draggable (draggable='true)) Only works on desktop
