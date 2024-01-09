3ec0f3dfb2d5bfec36f3e0237380e516cb92f0bb
Harpreet <harpreetsingh25102001@gmail.com>
2024-01-08 21:45:33 -0800

    async js

---
+++ 9-Master Asynchronous/1-Intro/index.js
@@ -0,0 +1,5 @@
+// Asynchronous programming is a technique that enables your program to start a potentially long-running task and still be able to be responsive to other events while that task runs, rather than having to wait until that task has finished.
+
+// Javascript is a single threaded language.
+
+// We can block the main thread with alert()
